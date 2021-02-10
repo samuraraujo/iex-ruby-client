b8d270e646c9b828878e3d068c4e2d400a19b987
Samur Araujo, PhD <samuraraujo@gmail.com>
2021-02-10 11:06:52 +0100

    Create splits.rb

---
+++ lib/iex/resources/splits.rb
@@ -0,0 +1,22 @@
+module IEX
+  module Resources
+    class Splits < Resource
+      property 'ex_date', from:'exDate'
+      property 'declared_date', from:'declaredDate'
+      property 'ratio'
+      property 'to_factor', from:'toFactor'
+      property 'from_factor', from:'fromFactor'
+      property 'description'
+      property 'symbol'
+      property 'id'
+      property 'source'
+      property 'key'
+      property 'subkey'
+      property 'date'
+      property 'updated'
+
+
+      
+    end
+  end
+end 
