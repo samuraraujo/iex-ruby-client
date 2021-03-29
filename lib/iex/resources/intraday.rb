b887ad9bc93a5f81eb38cab0027cdc93823bd0dc
Samur Araujo, PhD <samuraraujo@gmail.com>
2021-03-29 16:05:43 +0200

    Create intraday.rb

---
+++ lib/iex/resources/intraday.rb
@@ -0,0 +1,28 @@
+module IEX
+  module Resources
+    class Intraday < Resource
+      property 'date'
+      property 'minute'
+      property 'label'
+      property 'marketOpen'
+      property 'marketClose'
+      property 'marketHigh'
+      property 'marketLow'
+      property 'marketAverage'
+      property 'marketVolume'
+      property 'marketNotional'
+      property 'marketNumberOfTrades'
+      property 'marketChangeOverTime'
+      property 'high'
+      property 'low'
+      property 'open'
+      property 'close'
+      property 'average'
+      property 'volume'
+      property 'notional'
+      property 'numberOfTrades'
+      property 'changeOverTime'
+
+    end
+  end
+end
